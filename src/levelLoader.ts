09d619c5003c4a6b6fe08390c489006317d14f5f
Axwabo <axwabo@gmail.com>
2025-11-23 09:51:49 +0100

    use base64

--- src/levelLoader.ts
+++ src/levelLoader.ts
@@ -9,9 +9,8 @@ export default async function loadLevel(data: Promise<Blob>): Promise<Level> {
     const svg = zip.files["frame.svg"];
     if (!svg)
         throw new Error("Frame not found");
-    let svgBlob = await svg.async("blob");
     return {
-        image: URL.createObjectURL(await image.async("blob")),
-        frame: URL.createObjectURL(new Blob([ svgBlob ], { type: "image/svg" }))
+        image: `data:image/png;base64,${await image.async("base64")}`,
+        frame: `data:image/svg+xml;base64,${await svg.async("base64")}`
     };
 }
